8616d5d6771ac8ad83468e3eb30feaa86e9cb052
Girdzijauskaitė, Ona <ona.girdijauskaite@i2cat.net>
2022-10-04 17:34:52 +0200

    added changes

--- src/pages/AktiviteterPage/index.jsx
+++ src/pages/AktiviteterPage/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import { useNavigate } from 'react-router'
 import { DefaultRoutes } from "../../Routes";
 import { NavLink , useParams} from "react-router-dom";
@@ -7,26 +8,46 @@ import nessdance from '../../assets/nessdance.jpg'
 
 
 export const AktiviteterPage = ()=>{
-    // const navigate= useNavigate()
+    
+
+    const AktiviteterPage = () => {
+        const [allClasses, setAllClasses] = useState([])
+      const [ randClass, setRandClass] = useState()
+    //   const navigate = useNavigate()
+
+    const fetchClasses = async () => {
+        const responce = await axios.get('http://localhost:4000/api/v1/activities')
+        setAllClasses(responce.data)
+        setRandClass(responce.data[Math.floor(Math.random()*responce.data.length)]
+        )
+      }
+
+      useEffect(() => {
+        fetchClasses()
+      }, [])
+
     return (
+        
         <>
        <main className=' p-8 h-full bg-[#5E2E53]'>
         <h1 className='text-[#EAEAEA] text-[36px] text-left'>Aktiviteter</h1>
 
         <section className='mt-5 h-3/6  w-full relative rounded-2xl overflow-hidden'>
 
-        <img src={nessdance} alt=""  className='rounded-2xl object-cover  h-full w-full rounded-br-none ' />
+        <img  src={element.asset?.url || ''}
+        
+        alt=""  className='rounded-2xl object-cover  h-full w-full rounded-br-none ' />
 
         <div className='absolute bottom-0 bg-[#E1A1E9] w-full p-3 text-left rounded-tr-[50px]  text-[#000000] text-[20px]'>
           <div className='ml-4 font-poppins '> 
-          <h1 className='text-[18px]'>Junior Fitness Dance</h1>
-          <h1 className='text-[18px]'>10-12 år</h1>
+          <h1 className='text-[18px]'>{element.name}</h1>
+          <h1 className='text-[18px]'>{element.minAge.maxAge}</h1>
           </div>
 
         </div>
         </section>
 
-        <section className='mt-5 h-3/6  w-full relative rounded-2xl overflow-hidden'>
+        {/* <section className='mt-5 h-3/6  w-full relative rounded-2xl overflow-hidden'>
 
 <img src={nessdance} alt=""  className='rounded-2xl object-cover  h-full w-full rounded-br-none ' />
 
@@ -50,9 +71,9 @@ export const AktiviteterPage = ()=>{
   </div>
 
 </div>
-</section>
+</section> */}
         
        </main>
         </>
     )
-}
+}}
