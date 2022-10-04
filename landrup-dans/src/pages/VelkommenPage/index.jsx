09b64f2bbf35708d3c78e492feb945ef89bd0d67
Girdzijauskaitė, Ona <ona.girdijauskaite@i2cat.net>
2022-10-04 09:46:11 +0200

    add btn welcome

--- landrup-dans/src/pages/VelkommenPage/index.jsx
+++ landrup-dans/src/pages/VelkommenPage/index.jsx
@@ -11,11 +11,18 @@ export const VelkommenPage = ()=>{
     const navigate= useNavigate()
     return (
         <>
-       <main className='relative h-screen'>
+       <main className='relative h-screen flex items-center justify-center'>
        <img src={welcome} alt=""  className='object-cover h-screen ' />
 
-       <button className=' absolute bottom-0' onClick={()=>navigate(`${DefaultRoutes.AktiviteterPage}`)
-}>Klik me</button>
+       <button className='absolute bottom-5 w-[249px] p-4 class="block  
+       text-[24px] text-center font-bold 
+        text-[#EAEAEA] bg-[#5E2E53]  border-none focus:border-blue-500  
+        rounded-full   dark:focus:ring-[#9771f3] dark:focus:border-[#9771f3] hover:bg-[#dddbd4] 
+        focus:outline-none focus:ring focus:ring-[#9771f3]"' 
+        onClick={()=>navigate(`${DefaultRoutes.AktiviteterPage}`)
+}>Kom i gang
+
+</button>
        </main>
         </>
     )
