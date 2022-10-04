c1931b4f4897ef6acd99ebcf6641e8922432a8e7
Girdzijauskaitė, Ona <ona.girdijauskaite@i2cat.net>
2022-10-04 10:50:44 +0200

    add changes new

--- src/pages/VelkommenPage/index.jsx
+++ src/pages/VelkommenPage/index.jsx
@@ -13,11 +13,16 @@ export const VelkommenPage = ()=>{
         <>
        <main className='relative h-screen flex items-center justify-center'>
        <img src={welcome} alt=""  className='object-cover h-screen ' />
+       
+     
 
-       <button className='absolute bottom-5 w-[249px] p-4 class="block  
+       <h1 className='absolute bottom-25 text-[36px] text-[#5E2E53]'> Landrup <span className=' text-[#E1A1E9]'> Dance</span> </h1>
+       <hr className='w-48 h-12 absolute' /> 
+       
+       <button className='absolute bottom-14 w-[249px] p-4 class="block  
        text-[24px] text-center font-bold 
         text-[#EAEAEA] bg-[#5E2E53]  border-none focus:border-blue-500  
-        rounded-full   dark:focus:ring-[#9771f3] dark:focus:border-[#9771f3] hover:bg-[#dddbd4] 
+        rounded-lg   dark:focus:ring-[#9771f3] dark:focus:border-[#9771f3] hover:bg-[#dddbd4] 
         focus:outline-none focus:ring focus:ring-[#9771f3]"' 
         onClick={()=>navigate(`${DefaultRoutes.AktiviteterPage}`)
 }>Kom i gang
